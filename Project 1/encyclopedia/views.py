9653cb51f9d6910eff3311f13710edb2162dfed9
maximbrykin <34312280+maximbrykin@users.noreply.github.com>
2022-06-02 16:08:47 +0500

    Step 2

--- Project 1/encyclopedia/views.py
+++ Project 1/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
 import markdown2
@@ -12,6 +13,11 @@ def index(request):
 
 
 def entry_content(request, entry_name):
-    return render(request, "encyclopedia/entry.html", {
-        "entry_content": markdown2.markdown(util.get_entry(entry_name))
-    })
+    if util.get_entry(entry_name):
+        return render(request, "encyclopedia/entry.html", {
+            "entry_content": markdown2.markdown(util.get_entry(entry_name))
+         }) 
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry_content": "The requested page '" + entry_name + "' is not found."
+        })        
